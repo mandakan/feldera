ed9efa9c2859f42a19963326eaee617efbcbbb16
Gerd Zellweger <mail@gerdzellweger.com>
2023-11-22 20:42:00 -0800

    buid
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- crates/cli/build.rs
+++
@@ -1,6 +0,0 @@
-// Example custom build script.
-fn main() {
-    // copy assets to OUT_DIR
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let out_dir = std::path::Path::new(&out_dir);
-}
